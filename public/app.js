6cec617e05d047531f2d368c367470fb84770ac7
Jay Are Galinada <jayaregalinada@gmail.com>
2016-03-22 10:24:31 +0800

    Remove notification instead. Fixed #3. - Added @sendTo method for shorthand.

--- public/app.js
+++ public/app.js
@@ -4,46 +4,27 @@ app = angular.module('App', [])
     $scope.message = {}
     $scope.sending = false
 
-    $scope.sendNotif = function(form, event) {
-        event.preventDefault()
-        $log.info('::sendNotif [form, event]', form, event)
-        if (form.$valid) {
-            $scope.sending = true
-            $http({
-                url:  window.location.origin + '/notify',
-                method: 'POST',
-                data: $scope.message
-            }).then(function(success) {
-                $scope.sending = false
-                $scope.message = {}
-                $log.info('::sendNotif@success', success)
-            }, function(error) {
-                $scope.sending = false
-                $scope.message = {}
-                $log.error('::sendNotif@error', error)
-            })
-        }
+    $scope.sendTo = function(method, url) {
+        $scope.sending = true
+        $http({
+            url: window.location.origin + '/' + url,
+            method: 'POST',
+            data: $scope.message
+        }).then(function(success) {
+            $scope.sending = false
+            $scope.message = {}
+            $log.info('::' + method + '@success', success)
+        }, function(error) {
+            $scope.sending = false
+            $scope.message = {}
+            $log.error('::' + method + '@error', error)
+        })
     }
 
     $scope.sendOnly = function(form, event) {
         event.preventDefault()
         $log.info('::sendOnly [form, event]', form, event)
-        if (form.$valid) {
-            $scope.sending = true
-            $http({
-                url:  window.location.origin + '/send',
-                method: 'POST',
-                data: $scope.message
-            }).then(function(success) {
-                $scope.sending = false
-                $scope.message = {}
-                $log.info('::sendOnly@success', success)
-            }, function(error) {
-                $scope.sending = false
-                $scope.message = {}
-                $log.error('::sendOnly@error', error)
-            })
-        }
+        if (form.$valid) $scope.sendTo('sendOnly', 'send')
     }
 
     $scope.sendAndShut = function(form, event) {
@@ -51,41 +32,13 @@ app = angular.module('App', [])
         $log.info('::sendAndShut [form, event]', form, event)
         if (form.$valid) {
             var confirming = $window.confirm('Are you sure you want to shutdown the computer?')
-            if (confirming) {
-                $scope.sending = true
-                $http({
-                    url: window.location.origin + '/shutdown',
-                    method: 'POST',
-                    data: $scope.message
-                }).then(function(success) {
-                    $scope.sending = false
-                    $scope.message = {}
-                    $log.info('::sendAndShut@success', success)
-                }, function(error) {
-                    $scope.sending = false
-                    $scope.message = {}
-                    $log.error('::sendAndShut@error', error)
-                })
-            }
+            if (confirming) $scope.sendTo('sendAndShut', 'shutdown')
         }
     }
 
     $scope.cancelShut = function(form, event) {
         event.preventDefault()
         $log.info('::cancelShut [form, event]', form, event)
-        if (form.$valid) {
-            $http({
-                url:  window.location.origin + '/cancel',
-                method: 'POST'
-            }).then(function(success) {
-                $scope.sending = false
-                $scope.message = {}
-                $log.info('::cancelShut@success', success)
-            }, function(error) {
-                $scope.sending = false
-                $scope.message = {}
-                $log.error('::cancelShut@error', error)
-            })
-        }
+        if (form.$valid) $scope.sendTo('cancelShut', 'cancel')
     }
 })
